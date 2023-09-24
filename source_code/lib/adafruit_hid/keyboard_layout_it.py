717344f1106c6c5746ba32086eb5eefed904c966
nønexist <111337838+n0nexist@users.noreply.github.com>
2023-09-24 15:52:08 +0200

    italian keyboard layout

---
+++ source_code/lib/adafruit_hid/keyboard_layout_it.py
@@ -0,0 +1,181 @@
+# SPDX-FileCopyrightText: 2017 Dan Halbert for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+`adafruit_hid.keyboard_layout_us.KeyboardLayoutUS`
+=======================================================
+
+* Author(s): Dan Halbert
+"""
+
+from .keyboard_layout_base import KeyboardLayoutBase
+
+
+class KeyboardLayoutIT(KeyboardLayoutBase):
+    """Map ASCII characters to appropriate keypresses on a standard US PC keyboard.
+
+    Non-ASCII characters and most control characters will raise an exception.
+    """
+
+    # The ASCII_TO_KEYCODE bytes object is used as a table to maps ASCII 0-127
+    # to the corresponding # keycode on a US 104-key keyboard.
+    # The user should not normally need to use this table,
+    # but it is not marked as private.
+    #""" """
+    # Because the table only goes to 127, we use the top bit of each byte (ox80) to indicate
+    # that the shift key should be pressed. So any values 0x{8,9,a,b}* are shifted characters.
+    #
+    # The Python compiler will concatenate all these bytes literals into a single bytes object.
+    # Micropython/CircuitPython will store the resulting bytes constant in flash memory
+    # if it's in a .mpy file, so it doesn't use up valuable RAM.
+    #
+    # \x00 entries have no keyboard key and so won't be sent.
+    ASCII_TO_KEYCODE = (
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x2a'  # BACKSPACE
+        b'\x2b'  # '\t'
+        b'\x28'  # '\n'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x29'  # ESC
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x00'
+        b'\x2c'  # ' '
+        b'\x9e'  # '!'
+        b'\x9f'  # '"'
+        b'\x34'  # '#'
+        b'\xa1'  # '$'
+        b'\xa2'  # '%'
+        b'\xa3'  # '&'
+        b'\x2d'  # "'"
+        b'\xa5'  # '('
+        b'\xa6'  # ')'
+        b'\xb0'  # '*'
+        b'\x30'  # '+'
+        b'\x36'  # ','
+        b'\x38'  # '-'
+        b'\x37'  # '.'
+        b'\xa4'  # '/'
+        b'\x27'  # '0'
+        b'\x1e'  # '1'
+        b'\x1f'  # '2'
+        b'\x20'  # '3'
+        b'\x21'  # '4'
+        b'\x22'  # '5'
+        b'\x23'  # '6'
+        b'\x24'  # '7'
+        b'\x25'  # '8'
+        b'\x26'  # '9'
+        b'\xb7'  # ':'
+        b'\xb6'  # ';'
+        b'\x64'  # '<'
+        b'\xa7'  # '='
+        b'\xe4'  # '>'
+        b'\xad'  # '?'
+        b'\x33'  # '@'
+        b'\x84'  # 'A'
+        b'\x85'  # 'B'
+        b'\x86'  # 'C'
+        b'\x87'  # 'D'
+        b'\x88'  # 'E'
+        b'\x89'  # 'F'
+        b'\x8a'  # 'G'
+        b'\x8b'  # 'H'
+        b'\x8c'  # 'I'
+        b'\x8d'  # 'J'
+        b'\x8e'  # 'K'
+        b'\x8f'  # 'L'
+        b'\x90'  # 'M'
+        b'\x91'  # 'N'
+        b'\x92'  # 'O'
+        b'\x93'  # 'P'
+        b'\x94'  # 'Q'
+        b'\x95'  # 'R'
+        b'\x96'  # 'S'
+        b'\x97'  # 'T'
+        b'\x98'  # 'U'
+        b'\x99'  # 'V'
+        b'\x9a'  # 'W'
+        b'\x9b'  # 'X'
+        b'\x9c'  # 'Y'
+        b'\x9d'  # 'Z'
+        b'\x2f'  # '['
+        b'\x35'  # '\\'
+        b'\x30'  # ']'
+        b'\xae'  # '^'
+        b'\xb8'  # '_'
+        b'\x00'
+        b'\x04'  # 'a'
+        b'\x05'  # 'b'
+        b'\x06'  # 'c'
+        b'\x07'  # 'd'
+        b'\x08'  # 'e'
+        b'\x09'  # 'f'
+        b'\x0a'  # 'g'
+        b'\x0b'  # 'h'
+        b'\x0c'  # 'i'
+        b'\x0d'  # 'j'
+        b'\x0e'  # 'k'
+        b'\x0f'  # 'l'
+        b'\x10'  # 'm'
+        b'\x11'  # 'n'
+        b'\x12'  # 'o'
+        b'\x13'  # 'p'
+        b'\x14'  # 'q'
+        b'\x15'  # 'r'
+        b'\x16'  # 's'
+        b'\x17'  # 't'
+        b'\x18'  # 'u'
+        b'\x19'  # 'v'
+        b'\x1a'  # 'w'
+        b'\x1b'  # 'x'
+        b'\x1c'  # 'y'
+        b'\x1d'  # 'z'
+        b'\xaf'  # '{'
+        b'\xb5'  # '|'
+        b'\xb0'  # '}'
+        b'\x00'
+        b'\x00'
+    )
+    NEED_ALTGR = '#@[]€{}'
+    HIGHER_ASCII = {
+        0xa3: 0xa0,  # '£'
+        0x20ac: 0x22,  # '€'
+        0xec: 0x2e,  # 'ì'
+        0xe8: 0x2f,  # 'è'
+        0xe9: 0xaf,  # 'é'
+        0xf2: 0x33,  # 'ò'
+        0xe7: 0xb3,  # 'ç'
+        0xe0: 0x34,  # 'à'
+        0xb0: 0xb4,  # '°'
+        0xf9: 0x31,  # 'ù'
+        0xa7: 0xb1,  # '§'
+    }
+    COMBINED_KEYS = {
+    }
+
+KeyboardLayout = KeyboardLayoutIT
